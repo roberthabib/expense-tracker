a77b0bd3de8b54d901ddfc4e96a1c4ea2f455e1f
roberthabib <rober.habib@gmail.com>
2026-03-30 12:50:40 +0300

    Bump service worker cache version to force refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'expense-tracker-v1';
+const CACHE = 'expense-tracker-v2';
 const SHELL = ['/', '/index.html', '/manifest.json', '/android-chrome-192x192.png', '/android-chrome-512x512.png'];
 
 self.addEventListener('install', e => {
